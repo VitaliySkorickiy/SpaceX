3c1e80f9c6953d0312f073c182ba3cc590042c11
zarias2020 <62381691+zarias2020@users.noreply.github.com>
2021-09-29 17:47:23 +0300

    Updates

---
+++ precache-manifest.dc16581721f1f8fc328eb628b8eb7ad5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5548d9e57524cb966a6e549be8d9865",
+    "url": "/SpaceX/index.html"
+  },
+  {
+    "revision": "af7cdd10a8befdc13e7c",
+    "url": "/SpaceX/static/css/main.593ebc82.chunk.css"
+  },
+  {
+    "revision": "552d9759fd57edb76dcd",
+    "url": "/SpaceX/static/js/2.bf8959ba.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/SpaceX/static/js/2.bf8959ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af7cdd10a8befdc13e7c",
+    "url": "/SpaceX/static/js/main.8f3ee8e3.chunk.js"
+  },
+  {
+    "revision": "dbbc612723065eb6204d",
+    "url": "/SpaceX/static/js/runtime-main.60a47493.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/SpaceX/static/media/logo.d5ab31ce.svg"
+  }
+]);
